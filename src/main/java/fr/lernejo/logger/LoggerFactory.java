e980bda92823d2e1f687ef2ef44c10ad9f4e3406
enobil franklin claire <enobilfranklin@et.esiea.fr>
2022-10-10 12:28:40 +0200

    part6

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,6 +3,6 @@ package fr.lernejo.logger;
 public class LoggerFactory {
 
     public static Logger getLogger(String name){
-        return new ContextualLogger(new FileLogger("logger.log"),name);
+        return new ContextualLogger(new CompositeLogger(new FileLogger("logger.log"), new ConsoleLogger()),name);
     }
 }
